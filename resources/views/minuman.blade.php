d139343046861a6ac7a0fe87e9b0f9ccf6b6a3b5
AriSaputra2 <arisaputra01062002@gmail.com>
2022-04-22 18:04:29 +0700

    first

--- resources/views/minuman.blade.php
+++ resources/views/minuman.blade.php
@@ -1,14 +1,4 @@
-@extends('layout.main')
+@extends('Layout.main')
 @section('section')
-    <!-- Section Background Scooter-->
-    <section class="text-center">
-        <div class="jumbotron jumbotron-fluid jumbotron-minuman">
-          <div class="container content ">
-            <h1 class="display-4 fs-1 text-light " data-aos="zoom-out-up">Our Foodpedia</h1>
-            <p class="text-white" data-aos="zoom-out-up" data-aos-delay="50"><a href="/minuman" class="btn btn-dark text-decoration-none"> Let's Check It</a></p>
-          </div>
-        </div>
-        {{-- <p class="fw-bold fs-4 text-light"><?= waktu(); ?> Skut !!!</p> --}}      
-      </section>
-
+    
 @endsection
