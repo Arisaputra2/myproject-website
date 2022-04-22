0ce10c444f3e75f53c149d8feca4a3397b948f26
AriSaputra2 <arisaputra01062002@gmail.com>
2022-04-22 17:40:08 +0700

    minuman

--- resources/views/minuman.blade.php
+++ resources/views/minuman.blade.php
@@ -3,9 +3,9 @@
     <!-- Section Background Scooter-->
     <section class="text-center">
         <div class="jumbotron jumbotron-fluid jumbotron-minuman">
-          <div class="container content">
+          <div class="container content ">
             <h1 class="display-4 fs-1 text-light " data-aos="zoom-out-up">Our Foodpedia</h1>
-            <p class="text-white" data-aos="zoom-out-up" data-aos-delay="50"><a href="#food" class="btn btn-dark text-decoration-none"> Let's Check It</a></p>
+            <p class="text-white" data-aos="zoom-out-up" data-aos-delay="50"><a href="/minuman" class="btn btn-dark text-decoration-none"> Let's Check It</a></p>
           </div>
         </div>
         {{-- <p class="fw-bold fs-4 text-light"><?= waktu(); ?> Skut !!!</p> --}}      
